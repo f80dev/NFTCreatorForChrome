97712c64ca9279109a77b8536b79a8e73dae2ded
F80 <43803343+f80dev@users.noreply.github.com>
2025-02-17 22:24:06 +0100

    commit pour publication production

--- src/environments/settings_devnet.ts
+++ src/environments/settings_devnet.ts
@@ -2,6 +2,6 @@ export const settings={
   contract_addr:"",
   ihm_level:2,
   appname: "NFT Factory",
-  appli:"https://nft.f80.fr",
+  appli:"https://devnet.nft.f80.fr",
   network: "elrond-devnet"
 }
