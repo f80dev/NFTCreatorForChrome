9776f7d3f1b7c74153ce8266a10dd0014bb145c8
hhoar <hhoareau@gmail.com>
2025-02-20 16:59:14 +0100

    commit pour publication devnet

--- deploy_prod.bat
+++ deploy_prod.bat
@@ -1,6 +1,6 @@
 copy .\src\CNAME_prod .\src\CNAME
 copy .\src\index_prod.html .\src\index.html
-copy .\src\manifest-prod.webmanifest .\src\manisfest.webmanifest
+copy .\src\manifest-production.webmanifest .\src\manisfest.webmanifest
 git commit -a -m "commit pour publication production"
 call npm version patch
 call ng build --aot --output-hashing=none --source-map=false --optimization=true --configuration production
