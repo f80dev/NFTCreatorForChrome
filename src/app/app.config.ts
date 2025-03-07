630104ed8f38e6c762223a569856949688e71ec1
F80 <43803343+f80dev@users.noreply.github.com>
2025-03-07 20:10:12 +0100

    commit pour publication devnet

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -52,7 +52,10 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
 
   ]
 };
