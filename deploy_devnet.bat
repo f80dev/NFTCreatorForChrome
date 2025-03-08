37fc0698e4f7b62e6c107c2a0835de28caaa7ad7
hhoar <hhoareau@gmail.com>
2025-03-08 13:44:11 +0100

    commit pour publication devnet

--- deploy_devnet.bat
+++ deploy_devnet.bat
@@ -3,4 +3,4 @@ copy .\src\CNAME_devnet .\src\CNAME
 copy .\src\index_devnet.html .\src\index.html
 copy .\src\manifest-devnet.webmanifest .\src\manifest.webmanifest
 call ng build --aot --output-hashing=none --source-map=false --optimization=true --configuration development
-call gh-pages -d ./dist --repo https://github.com/f80dev/nftfactory_devnet.git -f -t true -b gh-pages -m \"update from main\"",
+call gh-pages -d ./dist --repo https://github.com/f80dev/NFTNowDevnet.git -f -t true -b master -m \"update from devnet\"",
