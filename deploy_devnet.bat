5eb2053bba39ac34311073330ea4af12d38e8764
F80 <43803343+f80dev@users.noreply.github.com>
2025-03-15 19:11:42 +0100

    commit pour publication devnet

--- deploy_devnet.bat
+++ deploy_devnet.bat
@@ -3,5 +3,5 @@ copy .\src\CNAME_devnet .\src\CNAME
 copy .\src\index_devnet.html .\src\index.html
 copy .\src\environments\settings_devnet.ts .\src\environments\settings.ts
 copy .\src\manifest-devnet.webmanifest .\src\manifest.webmanifest
-call ng build --aot --output-hashing=all --source-map=false --optimization=false --configuration development
+call ng build --aot --output-hashing=all --source-map=false --optimization=true --configuration development
 call gh-pages -d ./dist --repo https://github.com/f80dev/NFTNowDevnet.git -f -t true -b master -m \"update from devnet\"",
