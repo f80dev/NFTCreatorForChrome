41b26117d007a5193600418edbbc25414d7c89ab
hhoar <hhoareau@gmail.com>
2025-03-17 01:37:34 +0100

    commit pour publication production

--- src/environments/settings.ts
+++ src/environments/settings.ts
@@ -1,11 +1,19 @@
 export const settings={
   contract_addr:"",
-  ihm_level:2,
+  ihm_level:1,
+  appname: "NFTNow",
   version: "0.8",
-  appname: "NFTNow Devnet",
-  appli:"https://devnet.nftnow.af10.fr",
-  network: "elrond-devnet",
-  intro: "Create your NFT on the devnet network in one minute"
+  appli:"https://nftnow.af10.fr",
+  network: "elrond-mainnet",
+  intro: "Create your NFT in one minute"
 }
 
-export const abi={}
+export const abi=[
+  "constructor(string memory _name, string memory _symbol, string memory _baseURI)",
+  "function mint(address recipient) public returns (uint256)",
+  "function setBaseURI(string memory _newBaseURI) public",
+  "function _baseURI() internal view virtual override returns (string memory)",
+  "function baseURI() external view returns (string memory)",
+  "function name() external view returns (string memory)",
+  "function symbol() external view returns (string memory)",
+];
