72bfd1cbd7f07b17e694464b424b10ca91d3006f
F80 <43803343+f80dev@users.noreply.github.com>
2025-03-17 12:59:40 +0100

    commit pour publication devnet

--- src/environments/settings_devnet.ts
+++ src/environments/settings_devnet.ts
@@ -1,7 +1,7 @@
 export const settings={
   contract_addr:"",
   ihm_level:2,
-  version: "0.9",
+  version: "0.91",
   appname: "NFTNow Devnet",
   appli:"https://devnet.nftnow.af10.fr",
   network: "elrond-devnet",
